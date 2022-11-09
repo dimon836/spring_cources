384e63bc353323899c680d3e034acf8a4f6a8f06
dimon836 <65618206+dimon836@users.noreply.github.com>
2022-11-09 17:42:21 +0200

    add all files

---
+++ src/main/java/com/example/spring_cources/SpringCoursesApplication.java
@@ -0,0 +1,12 @@
+package com.example.spring_cources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoursesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCoursesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring_cources/SpringCoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_cources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoursesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
